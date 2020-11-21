57aa0e9579e80ff2ec7bfd3a3ae6a20f17da69ec
yaswanth3610 <yaswanthkrishna.g@gmai.com>
2020-11-21 15:30:26 +0530

    circular buffer demo

---
+++ CircularBuff/.circularbuff.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/yesh/DeviceDrivers/CircularBuff/circularbuff.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/yesh/DeviceDrivers/CircularBuff/circularbuff.ko /home/yesh/DeviceDrivers/CircularBuff/circularbuff.o /home/yesh/DeviceDrivers/CircularBuff/circularbuff.mod.o;  true
---
+++ CircularBuff/.circularbuff.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/yesh/DeviceDrivers/CircularBuff/circularbuff.mod := { echo  /home/yesh/DeviceDrivers/CircularBuff/circularbuff.o; echo; } > /home/yesh/DeviceDrivers/CircularBuff/circularbuff.mod
