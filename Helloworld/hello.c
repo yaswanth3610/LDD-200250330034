b5cd9a5654522efa1ad5e86ec01734b575eaa94c
yaswanth3610 <yaswanthkrishna.g@gmai.com>
2020-11-24 11:02:05 +0530

    module initalization and exiting demo

--- Helloworld/hello.c
+++ Helloworld/hello.c
@@ -2,13 +2,13 @@
 
 MODULE_LICENSE("GPL");
 
-static int __init hello_init(void)
+static int __init hello_init(void //initalization)
 {
 	printk("HI,i am in init to kernal module\n");
 	return 0;
 }
 
-static void __exit hello_exit(void)
+static void __exit hello_exit(void)//exiting
 {
 	printk("BYE,i am exiting from kernal module\n");
 }
